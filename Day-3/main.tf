14e973b1f1711226b3f79605bfb8c7215de6f3f6
nagariyaarchit <nagariyaarchit@gmail.com>
2025-05-01 12:25:56 -0400

    Added key pair

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0f88e80871fd81e91" 
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-0c6dc0029c3c7bbc4" 
+  key_pair = "MyKeyPair"
 }
